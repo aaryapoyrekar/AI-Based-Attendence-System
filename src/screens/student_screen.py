2f8c0336c2159aa4bee5b041e609ca81a0fc3caf
aaryapoyrekar <aaryapoyrekar1226@gmail.com>
2026-07-25 00:20:29 +0530

    voice pipeline adition

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -4,6 +4,7 @@ from src.components.header import header_dashboard
 from src.components.footer import footer_dashboard
 from PIL import Image
 from src.ui.base_layout import style_base_layout, style_background_dashboard
+from src.pipelines.face_pipeline import predict_attendence
 
 def student_screen():
     style_background_dashboard()
@@ -22,6 +23,14 @@ def student_screen():
     photo_source = st.camera_input("Position your face in the center")
 
     if photo_source:
-        np.array(Image.open(photo_source))
+        img = np.array(Image.open(photo_source))
+
+        with st.spinner('AI is scanning...'):
+            detected,all_ids, num_faces = predict_attendence(img)
+
+            if num_faces == 0:
+                st.warning('Face not found!')
+            
+
 
     footer_dashboard()

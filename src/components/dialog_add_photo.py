944e97aba682f1a46fb9b701a15c1e95c8e21dd0
aaryapoyrekar <aaryapoyrekar1226@gmail.com>
2026-07-29 00:17:26 +0530

    photo upload festure

---
+++ src/components/dialog_add_photo.py
@@ -0,0 +1,34 @@
+import streamlit as st 
+from src.database.db import enroll_student_to_subject
+from src.database.config import supabase
+from PIL import Image
+import time 
+
+@st.dialog("Capture or Upload photos ")
+def add_photos_dialog():
+    st.write('Add classroom photos to scan for attendence')
+
+    if 'photo_tab' not in st.session_state:
+        st.session_state.photo_tab = 'camera'
+
+    t1, t2 = st.columns(2)
+
+    with t1:
+        type_camera = 'primary' if st.session_state.photo_tab == 'camera' else 'tertiary'
+        if st.button('Camera', type=type_camera, width='stretch'):
+            st.session_state.photo_tab = 'camera'
+
+    with t1:
+        type_upload = 'primary' if st.session_state.photo_tab == 'upload' else 'tertiary'
+        if st.button('Upload Photos', type=type_camera, width='stretch'):
+            st.session_state.photo_tab = 'upload'
+
+    if st.session_state.photo_tab == 'camera':
+        cam_photo = st.camera_imput('Take Snapshot', key='dialog_cam')
+        if cam_photo:
+            st.session_state.attendence_images.append(Image.open(cam_photo))
+            st.toast('Photo Captured')
+            st.rerun()
+
+    if st.session_state.photo_tab == 'upload':
+        uploaded_files = st.file_uploader('Choose image files', type=['jpg', 'png', 'jpeg'])
--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -73,7 +73,35 @@ def teacher_dashboard():
     footer_dashboard()
 
 def teacher_tab_take_attendence():
+    teacher_id = st.session_state.teacher_data['teacher_id']
     st.header('Take AI Attendence')
+
+    if 'attendence_images' not in st.session_state:
+        st.session_state.attendence_images = []
+
+    subjects = get_teacher_subjects(teacher_id)
+
+    if not subjects:
+        st.warning('You havent created any subjects yet! Please creat one to begin!')
+        return
+    subject_options = {f"{s['name']} - {s['subject_code']}": s['subject_id'] for s in subjects}
+
+    col1, col2 = st.columns([3,1])
+
+    with col1:
+        selected_subjects_label = st.selectbox('Select Subject', options=list(subject_options.keys()))
+
+    with col2:
+        if st.button('Add Photos', type='primary', icon=':material/photo_prints:', width='stretch'):
+            add_photo_dialog()
+
+    selected_subject_id = subject_options[selected_subjects_label]
+
+    st.divider()
+
+
+
+
 
 def teacher_tab_manage_subjects():
     teacher_id = st.session_state.teacher_data['teacher_id']
